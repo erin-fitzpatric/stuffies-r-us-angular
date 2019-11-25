2ca41c183d8b4256201b6cfe70173b697b85cf67
erin-fitzpatric <efitzpatric@gmail.com>
2019-11-25 14:38:34 -0500

    init commit

---
+++ src/app/model/json-response.class.ts
@@ -0,0 +1,12 @@
+export class JsonResponse {
+    data: object;
+    errors: object;
+    meta: object;
+
+
+    constructor(data: object, errors: object, meta: object) {
+        this.data = data;
+        this.errors = errors;
+        this.meta = meta;
+    }
+}
---
+++ src/app/model/stuffy.class.ts
@@ -0,0 +1,15 @@
+export class Stuffy {
+    id: number; 
+    type: string;
+    color: string; 
+    size: string; 
+    limbs: number;
+
+    constructor(id: number = null, type: string="", color: string="", size: string="", limbs: number= null) {
+        this.id = id;
+        this.type =type;
+        this.color =color;
+        this.size =size;
+        this.limbs =limbs;
+    }
+}
